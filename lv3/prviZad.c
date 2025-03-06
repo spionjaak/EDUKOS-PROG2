de6f8d4b5adf698108b08c2b53de7861289ca449
spionjaak <spionjaak@gmail.com>
2025-03-06 20:32:11 +0100

    Update prviZad.c

--- lv3/prviZad.c
+++ lv3/prviZad.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main(){
 
+char *mjeseci[]={ "sijecanj", "veljaca", "ozujak", "travanj", "svibanj", "lipanj", "srpanj", "kolovoz", "rujan", "listopad", "studeni", "prosinac"};
+int mjesec;
+
+//Ovdje ide vas kod
+
+scanf("%d",&mjesec);
+printf("REZULTATI:\n");
+if(mjesec>12||mjesec<1)printf("Nekorektan broj mjeseca");
+else printf("%s",*(mjeseci+mjesec-1));
+
+return 0;
+}
