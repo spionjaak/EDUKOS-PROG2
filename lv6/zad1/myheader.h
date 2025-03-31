37f23952f95263b3e496cce40b682fc3df1fea8d
spionjaak <spionjaak@gmail.com>
2025-03-31 22:06:46 +0200

    Create myheader.h

---
+++ lv6/zad1/myheader.h
@@ -0,0 +1,8 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+int** allocateMatrix(int n,int m);
+void inputMatrix(int **M,int n,int m);
+void matrixTimesK(int **M,int n,int m,int K);
+void printMatrix(int **M,int n,int m);
+#endif
